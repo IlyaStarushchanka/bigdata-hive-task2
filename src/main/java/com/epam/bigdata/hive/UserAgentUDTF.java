5062d2b487b746122b21a70dbed2e1d34e136c0e
Ilya_Starushchanka <ilya10194@mail.ru>
2016-09-20 15:15:30 +0300

    Write UDTF for reading information from userAgent

--- src/main/java/com/epam/bigdata/hive/UserAgentUDTF.java
+++ src/main/java/com/epam/bigdata/hive/UserAgentUDTF.java
@@ -1,7 +1,52 @@
 package com.epam.bigdata.hive;
 
+import eu.bitwalker.useragentutils.UserAgent;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+
+import java.util.ArrayList;
+
 /**
  * Created by Ilya_Starushchanka on 9/20/2016.
  */
-public class UserAgentUDTF extends GenericUDTF{
+public class UserAgentUDTF extends GenericUDTF {
+
+    private Object[] fwdObj = null;
+    private PrimitiveObjectInspector userAgentDtlOI = null;
+
+    @Override
+    public StructObjectInspector initialize (ObjectInspector[] arg){
+        ArrayList<String> fieldNames = new ArrayList<String>();
+        ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
+        userAgentDtlOI = (PrimitiveObjectInspector) arg[0];
+        fieldNames.add("UA_type");
+        fieldOIs.add(PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.STRING));
+        fieldNames.add("UA_family");
+        fieldOIs.add(PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.STRING));
+        fieldNames.add("OS_name");
+        fieldOIs.add(PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.STRING));
+        fieldNames.add("Device");
+        fieldOIs.add(PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.STRING));
+        fwdObj = new Object[4];
+        return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames,fieldOIs);
+    }
+
+    public void process(Object[] objects) throws HiveException {
+        final String userAgentString = userAgentDtlOI.getPrimitiveJavaObject(objects[0]).toString();
+        UserAgent userAgent = UserAgent.parseUserAgentString(userAgentString);
+        fwdObj[0] = userAgent.getBrowser().getBrowserType().getName();
+        fwdObj[1] = userAgent.getBrowser().getGroup().getName();
+        fwdObj[2] = userAgent.getOperatingSystem().getName();
+        fwdObj[3] = userAgent.getOperatingSystem().getDeviceType().getName();
+        forward(fwdObj);
+    }
+
+    public void close() throws HiveException {
+
+    }
 }
